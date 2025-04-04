9ffa8a693079fe6276fed67b6a73b9eb58ce5ffd
Ammar <ammar.lokh1234@gmail.com>
2025-04-04 06:22:36 -0400

    structure plugin files in correct order

---
+++ lua/ai-chat/init.lua
@@ -0,0 +1,7 @@
+print("init file loaded")
+local M = {}
+function M.print_on_key()
+	print("keypressed")
+end
+
+return M
---
+++ plugin/ai-chat.lua
@@ -0,0 +1,2 @@
+local map = vim.keymap
+map.set("n", "asdf", ":lua require('ai-chat').print_on_key()<CR>")
